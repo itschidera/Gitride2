aeda9205101bf640e3ce3efc4dc9026895d5d0fa
Temitoyin <tjayorinde@gmail.com>
2022-11-24 12:40:10 +0100

    section two

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import styles from "./App.module.scss"
+import Phone from "./assets/images/phone.png"
+import {stepData} from "./data"
 function App() {
   return (
     <div className={styles.App}>
       <section></section>
-      <section></section>
+      <section className={styles.App_sectionTwo}>
+        <div className={styles.App_sectionTwoHeader}>
+        <h2>Whichride brings together multiple ride hailing providers and local taxi firms in one app</h2>
+        <p>Allowing riders to select the provider that offers the best price, service or travel time, then seamlessly book within the app</p>
+        </div>
+        <div className={styles.App_sectionTwoContent}>
+          <div>
+            {/* <Phone /> */}
+            <img src={Phone}/>
+          </div>
+          <div>
+            {
+              stepData && stepData.map((e, i) => (
+                <div className={styles.App_sectionTwoContentStepItem} key={e.title}>
+                  <div className={styles.indicator}><p>{`Step ${i+1} `}</p> </div>
+                <div className={styles.App_sectionTwoContentStepItemInfo} >
+                  <h3>{e.title}</h3>
+                  <p>{e.subTitle}</p>
+                 </div> 
+                 </div>
+              ))
+            }
+          </div>
+        </div>
+  
+      </section>
       <section></section>
       <section></section>
     </div>
---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const stepData = [
+    {
+        title: "Enter location",
+        subTitle: "Enter your destination to see a list of taxi providers near you."
+    },
+    {
+        title: "Find the perfect ride",
+        subTitle: "Filter results by driver rating, driver sex, waiting times, price or speed. Reduce carbon footprint by only showing hybrid or electric cars."
+    },
+    {
+        title: "Pay and Save",
+        subTitle: "Once you select a ride, we search the web for coupons to get you an even better deal, then book seemlessly within our app and earn points while you ride."
+   
+    }
+];
